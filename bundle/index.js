7beb77de5fec59d54020481a13fe413751cc7f09
alfaruqii <bumbblechover@gmail.com>
2023-03-22 21:43:04 +0700

    different between interface with capital and not

--- bundle/index.js
+++ bundle/index.js
@@ -1,3 +1,9 @@
 "use strict";
 let myName = "alfaruqi";
+let absens = 123;
+let callMyName = () => {
+    console.log(myName);
+};
+console.log(typeof callMyName);
+console.log({ absens });
 console.log({ myName });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-let myName: string = "alfaruqi"
+let myName: String = "alfaruqi"
+let absens:Number = 123
+let callMyName:Function = () => {
+  console.log(myName)
+}
+
+console.log(typeof callMyName)
+
+console.log({absens})
 console.log({myName})
