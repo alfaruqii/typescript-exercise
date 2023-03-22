888ac2633512b6eb56b606e2b52966a07d27ddec
alfaruqii <bumbblechover@gmail.com>
2023-03-22 22:05:48 +0700

    different between interface with capital and not

--- bundle/index.js
+++ bundle/index.js
@@ -1,12 +1,14 @@
 "use strict";
 let myName = "alfaruqi";
 let yourName = "zahra";
-let absens = 123;
+let absens = 123; //usually use "number" instead of "Number" and also other type
+let data = ["hehe"];
 let callMyName = () => {
     console.log(myName);
     return myName;
 };
-console.log(typeof myName);
+console.log(data);
+console.log(myName);
 console.log({ callMyName });
 console.log({ absens });
 console.log({ myName });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 let myName: String = "alfaruqi"
 let yourName:string = "zahra"
-let absens:Number = 123
+let absens:number = 123 //usually use "number" instead of "Number" and also other type
+let data:Array<any> = ["hehe"]
 let callMyName:Function = () => {
   console.log(myName)
   return myName
 }
+console.log(data)
 
-console.log(typeof myName)
+
+console.log(myName)
 console.log({callMyName})
 
 console.log({absens})
