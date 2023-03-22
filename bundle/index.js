5ea28bd89b374b28cff943494714beec08d6f75a
alfaruqii <bumbblechover@gmail.com>
2023-03-23 00:58:06 +0700

    learning interfac

--- bundle/index.js
+++ bundle/index.js
@@ -6,6 +6,7 @@ let temen1 = {
     gender: "lelaki",
     hutang: 40000000
 };
+let zahra = [yourName, 17, "perempuan"];
 let absens = 123; //usually use "number" instead of "Number" and also "String" use "String"
 //array
 let data = ["hehe"];
@@ -21,11 +22,40 @@ let callMyName = () => {
     //if i return myName that i have type "String" not "string" it will have an error
     return yourName;
 };
+function CallSpec(spec) {
+    return `
+  This was your laptop specification,
+  ---
+  brand : ${spec.brand} ✨
+  model : ${spec.model} ✨
+  processor : ${spec.processor} ✨
+  display : ${spec.display} ✨
+  ---
+  Thats all your laptop specification, hope you like it ✨
+  `;
+}
 console.log(myName);
+console.log(zahra);
 console.log(callMyName());
-callMe(4, 5);
+const result = callMe(4, 5);
+console.log(result);
 console.log({ temen1 });
 console.log({ absens });
 console.log({ myName });
 console.log(data);
 console.log(newData);
+const laptopResult = CallSpec({
+    brand: "thinkpad",
+    model: "x220",
+    processor: {
+        brand: "intel",
+        core: {
+            totalCore: 12
+        },
+        clockspeed: true
+    },
+    display: {
+        typeDisplay: "1920x1080"
+    }
+});
+console.log(laptopResult);
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,41 @@
 let myName: String = "alfaruqi"
 let yourName:string = "zahra"
+interface Core {
+  totalCore:number
+}
+interface Processor {
+  brand:string,
+  core:Core
+  clockspeed:boolean
+}
+interface Display {
+  typeDisplay:string
+}
+interface Laptop {
+  brand:string,
+  model:string,
+  processor:Processor,
+  display:Display
+}
 type Temen = {
   name : string,
   gender : string,
   hutang? : number
 }
+type Gender = string
+
+type Pasangan = [
+  name:string,
+  age:number,
+  gender:Gender
+]
+
 let temen1:Temen = {
   name:"Dika",
   gender:"lelaki",
   hutang:40_000_000
 }
+let zahra:Pasangan = [yourName,17,"perempuan"]
 let absens:number = 123 //usually use "number" instead of "Number" and also "String" use "String"
 
 //array
@@ -27,9 +53,22 @@ let callMyName = () :string => {
   //if i return myName that i have type "String" not "string" it will have an error
   return yourName
 }
+function CallSpec(spec:Laptop):(Laptop | string){
+  return `
+  This was your laptop specification,
+  ---
+  brand : ${spec.brand} ✨
+  model : ${spec.model } ✨
+  processor : ${spec.processor} ✨
+  display : ${spec.display} ✨
+  ---
+  Thats all your laptop specification, hope you like it ✨
+  `
+}
 
 
 console.log(myName)
+console.log(zahra)
 console.log(callMyName())
 const result = callMe(4,5)
 console.log(result)
@@ -40,3 +79,18 @@ console.log({myName})
 
 console.log(data)
 console.log(newData)
+const laptopResult = CallSpec({
+  brand:"thinkpad",
+  model:"x220",
+  processor:{
+    brand:"intel",
+    core:{
+      totalCore : 12
+    },
+    clockspeed:true
+  },
+  display : {
+    typeDisplay:"1920x1080"
+  }
+})
+console.log(laptopResult)
